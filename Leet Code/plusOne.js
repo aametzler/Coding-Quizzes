54656b2e1342693011ed20f50648e037a1ad43a6
aametzler <adrianmetzler1@gmail.com>
2020-07-08 18:17:28 -0300

    added leet code problem

---
+++ Leet Code/plusOne.js
@@ -0,0 +1,16 @@
+/*
+* Easy - Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+*
+* The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+*
+* You may assume the integer does not contain any leading zero, except the number 0 itself.
+*/
+
+const plusOne = function(digits) {
+    for(let i = digits.length - 1; i >= 0; i--){
+      if(++digits[i] > 9) digits[i] = 0;
+      else return digits;
+    }
+    
+    return [1, ...digits];
+6};
